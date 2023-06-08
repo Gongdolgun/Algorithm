edc5c1c831bd302f74fee3ea5def7f6892785fc2
Minhak Lee <90584581+Gongdolgun@users.noreply.github.com>
2023-06-08 10:06:37 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 4032 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n, m, temp;
+	bool bIn = false;
+
+	vector<int> data;
+	vector<int> num;
+	vector<int> result;
+
+	cin >> n;
+
+	for(int i = 0; i < n; i++)
+	{
+		cin >> temp;
+		data.push_back(temp);
+	}
+	sort(data.begin(), data.end());
+
+	cin >> m;
+
+	for(int j = 0; j < m; j++)
+	{
+		cin >> temp;
+		num.push_back(temp);
+
+		bIn = binary_search(data.begin(), data.end(), temp);
+		result.push_back(bIn);
+	}
+
+	for(auto& Result : result)
+	{
+		cout << Result << '\n';
+	}
+
+	return 0;
+}

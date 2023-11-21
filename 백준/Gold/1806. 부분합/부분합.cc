1d76a2cec1516c9bfbac08080e48f0a1d3a53e3e
Minhak Lee <90584581+Gongdolgun@users.noreply.github.com>
2023-11-21 16:33:45 +0900

    [Gold IV] Title: 부분합, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+#define INF 987654321
+
+using namespace std;
+
+int n, s;
+vector<int> nums;
+
+void input()
+{
+	cin >> n >> s;
+	nums.resize(n);
+
+	for (int i = 0; i < n; i++)
+		cin >> nums[i];
+}
+
+void solution()
+{
+	// 2 pointer 알고리즘
+	// start ~ end의 값이 s보다 작으면 end를 한칸 올림
+	// start ~ end의 값이 s보다 크거나 같으면 start를 한칸 올림
+
+	int start_pos = 0, end_pos = 0;
+	int sum = nums[start_pos];
+	int answer = INF;
+
+	while (1)
+	{
+		if (sum < s)
+		{
+			end_pos++;
+			sum += nums[end_pos];
+		}
+		else
+		{
+			answer = min(answer, end_pos - start_pos + 1);
+			sum -= nums[start_pos];
+			start_pos++;
+		}
+
+		if (end_pos == nums.size() || answer == 1)
+			break;
+	}
+
+	if (answer == INF)
+	{
+		cout << 0;
+		return;
+	}
+
+	cout << answer;
+}
+
+int main()
+{
+	input();
+	solution();
+
+	return 0;
+}
